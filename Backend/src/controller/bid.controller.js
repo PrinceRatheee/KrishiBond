8d63414382e7294058a6dc18424f8c8be9d76e04
Sunil <sv784333@gmial.com>
2024-10-17 15:40:31 +0530

    Refactor createBidForFarmer controller function and route

--- Backend/src/controller/bid.controller.js
+++ Backend/src/controller/bid.controller.js
@@ -1,10 +1,11 @@
 import User from '../models/user.model.js';
 import Bid from '../models/bid.model.js';
 import CompanyDemand from '../models/companyDemand.model.js';
+
 const createBidForFarmer=async(req,res)=>{
     try{
         const {companyId}=req.params;
-        const {user,status,appliedFor,appliedRate,quantity,duration}=req.body;
+        const {user,status,appliedRate,quantity,duration}=req.body;
         const farmer=await User.findById(user);
         const companyDemand=await CompanyDemand.findById(companyId);
         if(!farmer){
@@ -16,14 +17,16 @@ const createBidForFarmer=async(req,res)=>{
        const bid=new Bid({
               user,
               status,
-              appliedFor,
+              appliedFor: companyId,
               appliedRate,
               quantity,
               duration  
         });
         await bid.save();
-        farmer.bids.push(bid);
+        farmer.bids.push(bid._id);
         await farmer.save();
+        companyDemand.bidsApplied.push(bid._id);
+        await companyDemand.save();
         res.status(201).json(bid);
     }catch(error){
         res.status(500).json({message:error.message});
