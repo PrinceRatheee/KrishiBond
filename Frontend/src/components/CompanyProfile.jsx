b664a4e8cfea63569b53b8864de956f33b966e93
shrey <shreyshukla101217@gmail.com>
2024-10-18 03:58:54 +0530

    random

--- Frontend/src/components/CompanyProfile.jsx
+++ Frontend/src/components/CompanyProfile.jsx
@@ -26,7 +26,7 @@ function CompanyProfile() {
     <div className='flex-col '>
         <div className="bg-gradient-to-r from-gray-900 via-black to-blue-900 min-h-screen text-white py-16 px-8 flex-col">
       <h2 className="text-4xl font-bold text-cyan-400 text-center mb-12">
-       Bid Now and Earn More !!
+        Your Listings
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {demands.length > 0 ? (
