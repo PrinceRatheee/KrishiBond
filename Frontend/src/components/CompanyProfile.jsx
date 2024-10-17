b0ac86dc35dba1b3e001342cfc594f19f6efd021
PrinceRatheee <rathi.prince2@gmail.com>
2024-10-18 00:01:35 +0530

    Profile Section started

---
+++ Frontend/src/components/CompanyProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CompanyProfile() {
+  return (
+    <div className='flex-col '>
+        <div>User Details:</div>
+        <div></div>
+    </div>
+  )
+}
+
+export default CompanyProfile
--- Frontend/src/components/FarmerProfile.jsx
+++ Frontend/src/components/FarmerProfile.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function FarmerProfile() {
-  
+  const [bids, setBids] = useState([]);
+
+  // Fetch company demands from the backend
+  useEffect(() => {
+    const fetchBids = async () => {
+      try {
+        const response = await axiosinstance.get("/api/companydemand/get");
+        setBids(response.data);
+        console.log("Bids fetched in farmer profile:", response.data);
+      } catch (error) {
+        console.error("Error fetching demands:", error);
+      }
+    };
+
+    fetchBids();
+  }, []);
+
   return (
     <div>FarmerProfile</div>
   )
