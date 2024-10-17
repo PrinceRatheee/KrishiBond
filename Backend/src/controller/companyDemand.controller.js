b7db5adda716a5413a89abc1b93edab542588633
Sunil <sv784333@gmial.com>
2024-10-17 16:09:29 +0530

    Refactor companyDemand.controller.js and companyDemand.route.js

--- Backend/src/controller/companyDemand.controller.js
+++ Backend/src/controller/companyDemand.controller.js
@@ -1,4 +1,5 @@
 import CompanyDemand from "../models/companyDemand.model.js";
+import Bid from "../models/bid.model.js";
 const getCompaniesDemands = async (req, res) => {
     try {
         const companies = await CompanyDemand.find();
@@ -33,4 +34,29 @@ const getCompamnyDemandsById=async(req,res)=>{
         res.status(404).json({message:error.message});
     }
 }
-export{ getCompaniesDemands,ListCompanyDemands,getCompamnyDemandsById};
+
+const approvedFarmerBids=async(req,res)=>{
+    const {bidId}=req.params;
+    const approved=req.body;
+    try{
+     const bid=await Bid.findById(bidId);
+        if(approved){
+            bid.status='approved';
+            const companyId=bid.appliedFor;
+            const companyDemand=await CompanyDemand.findById(companyId);
+            companyDemand.bidsAccepted.push(bidId);
+            companyDemand.quantityLeft-=bid.quantity;
+        }else{
+            bid.status='rejected';
+            const companyId=bid.appliedFor;
+            const companyDemand=await CompanyDemand.findById(companyId);
+            companyDemand.bidsApplied.pull(bidId);
+        }
+        await bid.save();
+        await companyDemand.save();
+        res.status(200).json(bid);
+    }catch(error){
+        res.status(404).json({message:error.message});
+    }
+}
+export{ getCompaniesDemands,ListCompanyDemands,getCompamnyDemandsById,approvedFarmerBids};
