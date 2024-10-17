18762f5d5f36c43c22787c4f128a5cc576a85acd
Sunil <sv784333@gmial.com>
2024-10-17 14:53:59 +0530

    Refactor companyDemand controller to add API endpoints for retrieving and creating company demands

--- Backend/src/controller/companyDemand.controller.js
+++ Backend/src/controller/companyDemand.controller.js
@@ -1,6 +1,24 @@
+import CompanyDemand from "../models/companyDemand.model.js";
 const getCompaniesDemands = async (req, res) => {
+    try {
+        const companies = await CompanyDemand.find();
+        res.status(200).json(companies);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 };
 const ListCompanyDemands=async(req,res)=>{
-    
+    const {companyID,crop,duration,quantity,rate}=req.body;
+
+    if(!companyID||!crop||!duration||!quantity||!rate){
+        return res.status(400).json({message:"All fields are required"});
+    }
+    const newCompanyDemand=new CompanyDemand({companyID,crop,duration,quantity,rate});
+    try{
+        await newCompanyDemand.save();
+        res.status(201).json(newCompanyDemand);
+    }catch{
+        res.status(409).json({message:error.message});
+    }
 }
 export{ getCompaniesDemands,ListCompanyDemands};
