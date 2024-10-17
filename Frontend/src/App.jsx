f31b6446d134379af7c51484856db263b8c5a324
Vaibhav <madaanvaibhav001@gmail.com>
2024-10-17 19:12:15 +0530

    made the all demands page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import AuthPage from "./pages/AuthPage";
 import DetailPage from "./pages/DetailPage";
 import CompanyDemandForm from "./pages/CompanyDemandForm.jsx";
 import Dashboard from "./components/Dashboard.jsx";
+import AllDemands from "./pages/AllDemands";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path="/detail" element={<DetailPage />} />
             <Route path="/create" element={< CompanyDemandForm/>} />
             <Route path="/profile" element={<Dashboard/>}/>
+            <Route path="/get" element={<AllDemands/>}/>
           </Routes>
         
      
