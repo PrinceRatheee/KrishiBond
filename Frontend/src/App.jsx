37e5626d8e60a62b0c1c77a8fdffc85da5e09eb5
Vaibhav <madaanvaibhav001@gmail.com>
2024-10-17 13:54:19 +0530

    redux setup and created both pages of farm and comp details

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import  AuthPage  from "./pages/AuthPage";
+import { Provider } from 'react-redux';
+import { store, persistor } from './Redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+
 function App() {
   return (
     <>
+     <Provider store={store}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<AuthPage/>} />
         </Routes>
       </BrowserRouter>
+      </Provider>
     </>
   );
 }
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => (
   <header className="p-6 flex justify-between items-center bg-black bg-opacity-70 shadow-lg">
-    <h1 className="text-4xl font-extrabold text-cyan-400">AgriChain</h1>
+    <h1 className="text-4xl font-extrabold text-cyan-400">Kisan Bond</h1>
     <nav className="space-x-6">
       <a href="#" className="text-lg hover:text-cyan-300 transition">Home</a>
       <a href="#" className="text-lg hover:text-cyan-300 transition">About</a>
