909834490ea8282cdbf025ec906edd296c652b89
Vaibhav <madaanvaibhav001@gmail.com>
2024-10-17 12:12:10 +0530

    landing page completed

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
+import Steps from "../components/Steps";
 import Footer from "../components/Footer";
+import FAQ from "../components/FAQ";
 
 const Home = () => (
   <div className="bg-gradient-to-r from-gray-900 via-black to-blue-900 min-h-screen text-white">
     <Header />
     <Hero />
     <Features />
+    <Steps />
+    <FAQ/>
     <Footer />
   </div>
 );
